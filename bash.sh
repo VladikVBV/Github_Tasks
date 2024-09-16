e6208e6e24b6914448939c972c4a5b18910ee04b
B. Vlad <vbvvlad@gmail.com>
2024-09-16 19:25:13 +0300

    make changes bash.sh

--- bash.sh
+++ bash.sh
@@ -5,3 +5,12 @@ do
    echo The counter is $COUNTER 
    let COUNTER=$COUNTER+1 
 done
+
+COUNTER2=0
+while [ $COUNTER2 -lt 100 ]
+do
+   echo The counter is $COUNTER2
+   let COUNTER2=$COUNTER2+5
+done
+
+
